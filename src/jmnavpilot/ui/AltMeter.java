7b20d98b207372e32bdba770d37f89c9ea00a2ac
Jesper Mathiassen <jesper.mathiassen@gmail.com>
2013-04-21 00:47:43 +0200

    Better altimeter

--- src/jmnavpilot/ui/AltMeter.java
+++ src/jmnavpilot/ui/AltMeter.java
@@ -15,15 +15,15 @@ import jmnavpilot.PlaneState;
  */
 public class AltMeter extends Instrument {
 
-    private final double maxAlt;
+    private double maxAlt;
     private double alt = 0;
 
     public AltMeter() {
         super();
         maxAlt = 100; // meters
-        alt = 10;
+        alt = 0;
     }
-    
+
     public AltMeter(double maxHeight) {
         super();
         this.maxAlt = maxHeight;
@@ -31,18 +31,33 @@ public class AltMeter extends Instrument {
 
     @Override
     public void paint(Graphics g) {
+        System.out.println("alt " + alt);
         // Dynamically calculate size information
         Dimension size = getSize();
 
         int y = (int) (size.height - (size.height * alt / maxAlt)) - 1;
 
-        // draw circle (color already set to foreground)
         g.setColor(Color.black);
+        g.fillRect(0, 0, size.width, size.height);
+        g.setColor(Color.white);
+        String msg = "Altitude [m]";
+        if (5 + g.getFontMetrics().getStringBounds(msg, g).getWidth() > size.width) {
+            msg = "Alt. [m]";
+        }
+        g.drawString(msg, 5, g.getFontMetrics().getHeight());
+        g.drawString(String.format("%3.1f", alt), 5, size.height / 2);
         g.drawLine(0, y, size.width, y);
     }
 
     @Override
     public void update(PlaneState state) {
         this.alt = state.getAlt();
+        if (alt > maxAlt) {
+            maxAlt = maxAlt * 10;
+        }
+        if (alt < maxAlt / 10 && maxAlt > 10) {
+            maxAlt = maxAlt / 10;
+        }
+        this.repaint();
     }
 }
